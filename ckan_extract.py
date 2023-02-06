af0f80e1df32a5deebaa2e299b1cb05d28774e88
Vincent Fazio <vjf@users.noreply.github.com>
2023-02-06 17:00:12 +1100

    Initial files

---
+++ ckan_extract.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+import json 
+from pathlib import Path
+import datetime
+import geojson
+from geojson import Polygon
+
+from pygeometa.core import read_mcf, render_j2_template
+from pygeometa.schemas.iso19139 import ISO19139OutputSchema
+
+
+
+def output_xml(ckan_dict, url):
+    # print(json.dumps(ckan_dict, indent=4))
+    try:
+        extent = geojson.loads(ckan_dict['GeoJSONextent'])
+        coords = extent.coordinates[0]
+        bbox = [coords[0][0], coords[1][1], coords[2][0], coords[3][1]]
+    except Exception:
+        bbox = [154.3, 109.1, -43.9, -10.6]
+    mcf_dict = {
+        "mcf": {
+            "version": 1.0
+        },
+        "metadata": {
+            "identifier": ckan_dict['id'],
+            "language": "en",
+            "charset": "utf8",
+            "parentidentifier": "",
+            "hierarchylevel": "dataset",
+            "datestamp": ckan_dict['metadata_modified'],
+            "dataseturi": url
+        },
+        "spatial": {
+            "datatype": "tin",
+            "geomtype": "composite"
+        },
+        "identification": {
+            "language": "eng; AUS",
+            "charset": "utf8",
+            "title": {
+                "en": ckan_dict['title'],
+            },
+            "abstract": {
+                "en": ckan_dict['notes'],
+            },
+            "dates": {
+                "creation": ckan_dict['metadata_created'],
+                "publication": ckan_dict['metadata_created']
+            },
+            "keywords": {
+                "default": {
+                    "keywords": {
+                        "en": [
+                            "geological models"
+                        ]
+                    }
+                }
+            },
+            "topiccategory": [
+                "geoscientificInformation"
+            ],
+            "extents": {
+                "spatial": [
+                    {
+                        "bbox": bbox,
+                        "crs": 4326
+                    }
+                ]
+            },
+            "fees": "None",
+            "accessconstraints": "license",
+            "rights": {
+                "en": "Copyright (c) 2010 Her Majesty the Queen in Right of Canada",
+            },
+            "url": url,
+            "status": "completed",
+            "maintenancefrequency": "continual"
+        },
+        "contact": {
+            "distributor": {
+                "organization": ckan_dict['organization']['title']
+            }
+        },
+        "distribution": {
+            "wms": {
+                "url": ckan_dict['resources'][0]['url'],
+                "type": "WWW:LINK",
+                "rel": "service",
+                "name": {
+                    "en": ckan_dict['resources'][0]['name'],
+                },
+                "description": {
+                    "en": ckan_dict['resources'][0]['resource:description'],
+                },
+                "function": "download"
+            }
+        },
+        "dataquality": {
+            "scope": {
+                "level": "dataset"
+            },
+            "lineage": {
+                "statement": f"This metadata record was reproduced from CKAN metadata retrieved from '{url}' on {datetime.datetime.now():%d %b %Y}"
+            }
+        }
+    }
+
+
+    # choose ISO 19139 output schema
+    iso_os = ISO19139OutputSchema()
+
+    ## default schema is pretty limited
+    #xml_string = iso_os.write(mcf_dict)
+
+    xml_string = render_j2_template(mcf_dict, template_dir='.')
+    print(xml_string)
+
+    # write to disk
+    with open('output.xml', 'w') as ff:
+        ff.write(xml_string)
+
+
+def get_ckan_dict(ckan_url, package_id):
+    url_path =  Path('api') / '3' / 'action' / 'package_show'
+    url = f'{ckan_url}/{url_path}'
+    # print("URL=", url)
+    r = requests.get(url, params={'q':'type:report', 'id':package_id})
+    try:
+        dict = json.loads(r.text)
+    except json.JSONDecodeError:
+        return {}, r.url
+    if dict['success'] is True:
+        return dict['result'], r.url
+    return {}, r.url
+
+if __name__ == "__main__":
+    site_url = 'https://geoscience.data.qld.gov.au'
+    ckan_dict, url = get_ckan_dict(site_url,'ds000002')
+    output_xml(ckan_dict, url)    
+
---
+++ extractm.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+from PyPDF2 import PdfReader
+import yake
+import spacy
+import pytextrank
+import glob
+import sys
+
+def parse_pdf(filename):
+    reader = PdfReader(filename)
+
+    meta = reader.metadata
+
+    print(len(reader.pages))
+
+    # All of the following could be None!
+    print(meta.author)
+    print(meta.creator)
+    print(meta.producer)
+    print(meta.subject)
+    print(meta.title)
+    print(meta)
+
+
+    text = ""
+    number_of_pages = len(reader.pages)
+    for page in reader.pages:
+        text += page.extract_text() + " "
+
+    return text
+
+#with open("MESAJ083011-019.txt", "r") as fd:
+#    text = fd.read()
+
+def run_yake(text):
+
+    print("yake:")
+    kw_extractor = yake.KeywordExtractor(top=100)
+    keywords = kw_extractor.extract_keywords(text)
+
+    for kw in keywords:
+        print(kw)
+
+def run_textrank(text):
+
+    print("textrank:")
+
+
+    # load a spaCy model, depending on language, scale, etc.
+    nlp = spacy.load("en_core_web_sm")
+
+    # add PyTextRank to the spaCy pipeline
+    nlp.add_pipe("textrank")
+    doc = nlp(text)
+
+    # examine the top-ranked phrases in the document
+    for phrase in doc._.phrases[:40]:
+        print(phrase.text)
+        print(phrase.rank, phrase.count)
+        print(phrase.chunks)
+        print()
+
+if __name__ == "__main__":
+    for file in glob.glob('*.pdf'):
+        text = parse_pdf(file)
+        run_yake(text)
+        run_textrank(text)
