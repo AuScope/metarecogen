c7ad45f9df27f3b78d3853441224cee64d50836a
Vincent Fazio <vjf@users.noreply.github.com>
2023-03-17 13:16:47 +1100

    Add classes for extraction from server

--- src/ckan_extract.py
+++ src/ckan_extract.py
@@ -6,132 +6,135 @@ import json
 from pathlib import Path
 import datetime
 import geojson
-from geojson import Polygon
 
-from pygeometa.core import read_mcf, render_j2_template
+from pygeometa.core import render_j2_template
 
+from extractor import Extractor
 
+class CkanExtractor(Extractor):
 
-def output_xml(ckan_dict, url):
-    # print(json.dumps(ckan_dict, indent=4))
-    try:
-        extent = geojson.loads(ckan_dict['GeoJSONextent'])
-        coords = extent.coordinates[0]
-        bbox = [coords[0][0], coords[1][1], coords[2][0], coords[3][1]]
-    except Exception:
-        bbox = [154.3, 109.1, -43.9, -10.6]
-    mcf_dict = {
-        "mcf": {
-            "version": 1.0
-        },
-        "metadata": {
-            "identifier": ckan_dict['id'],
-            "language": "en",
-            "charset": "utf8",
-            "parentidentifier": "",
-            "hierarchylevel": "dataset",
-            "datestamp": ckan_dict['metadata_modified'],
-            "dataseturi": url
-        },
-        "spatial": {
-            "datatype": "tin",
-            "geomtype": "composite"
-        },
-        "identification": {
-            "language": "eng; AUS",
-            "charset": "utf8",
-            "title": {
-                "en": ckan_dict['title'],
+    def output_xml(self, ckan_dict, url):
+        # print(json.dumps(ckan_dict, indent=4))
+        try:
+            extent = geojson.loads(ckan_dict['GeoJSONextent'])
+            coords = extent.coordinates[0]
+            bbox = [coords[0][0], coords[1][1], coords[2][0], coords[3][1]]
+        except Exception:
+            bbox = [154.3, 109.1, -43.9, -10.6]
+        mcf_dict = {
+            "mcf": {
+                "version": 1.0
             },
-            "abstract": {
-                "en": ckan_dict['notes'],
+            "metadata": {
+                "identifier": ckan_dict['id'],
+                "language": "en",
+                "charset": "utf8",
+                "parentidentifier": "",
+                "hierarchylevel": "dataset",
+                "datestamp": ckan_dict['metadata_modified'],
+                "dataseturi": url
             },
-            "dates": {
-                "creation": ckan_dict['metadata_created'],
-                "publication": ckan_dict['metadata_modified']
+            "spatial": {
+                "datatype": "tin",
+                "geomtype": "composite"
             },
-            "keywords": {
-                "default": {
-                    "keywords": {
-                        "en": [
-                            "geological models"
-                        ]
+            "identification": {
+                "language": "eng; AUS",
+                "charset": "utf8",
+                "title": {
+                    "en": ckan_dict['title'],
+                },
+                "abstract": {
+                    "en": ckan_dict['notes'],
+                },
+                "dates": {
+                    "creation": ckan_dict['metadata_created'],
+                    "publication": ckan_dict['metadata_modified']
+                },
+                "keywords": {
+                    "default": {
+                        "keywords": {
+                            "en": [
+                                "geological models"
+                            ]
+                        }
                     }
+                },
+                "topiccategory": [
+                    "geoscientificInformation"
+                ],
+                "extents": {
+                    "spatial": [
+                        {
+                            "bbox": bbox,
+                            "crs": 4326
+                        }
+                    ]
+                },
+                "fees": "None",
+                "accessconstraints": "license",
+                "rights": {
+                    "en": f"{ckan_dict['license_title']} ({ckan_dict['license_url']})",
+                },
+                "url": url,
+                "status": "completed",
+                "maintenancefrequency": "continual"
+            },
+            "contact": {
+                "distributor": {
+                    "organization": ckan_dict['organization']['title']
                 }
             },
-            "topiccategory": [
-                "geoscientificInformation"
-            ],
-            "extents": {
-                "spatial": [
-                    {
-                        "bbox": bbox,
-                        "crs": 4326
-                    }
-                ]
+            "distribution": {
+                "wms": {
+                    "url": ckan_dict['resources'][0]['url'],
+                    "type": "WWW:LINK",
+                    "rel": "service",
+                    "name": {
+                        "en": ckan_dict['resources'][0]['name'],
+                    },
+                    "description": {
+                        "en": ckan_dict['resources'][0]['resource:description'],
+                    },
+                    "function": "download"
+                }
             },
-            "fees": "None",
-            "accessconstraints": "license",
-            "rights": {
-                "en": f"{ckan_dict['license_title']} ({ckan_dict['license_url']})",
-            },
-            "url": url,
-            "status": "completed",
-            "maintenancefrequency": "continual"
-        },
-        "contact": {
-            "distributor": {
-                "organization": ckan_dict['organization']['title']
-            }
-        },
-        "distribution": {
-            "wms": {
-                "url": ckan_dict['resources'][0]['url'],
-                "type": "WWW:LINK",
-                "rel": "service",
-                "name": {
-                    "en": ckan_dict['resources'][0]['name'],
+            "dataquality": {
+                "scope": {
+                    "level": "dataset"
                 },
-                "description": {
-                    "en": ckan_dict['resources'][0]['resource:description'],
-                },
-                "function": "download"
-            }
-        },
-        "dataquality": {
-            "scope": {
-                "level": "dataset"
-            },
-            "lineage": {
-                "statement": f"This metadata record was reproduced from CKAN metadata retrieved from '{url}' on {datetime.datetime.now():%d %b %Y}"
+                "lineage": {
+                    "statement": f"This metadata record was reproduced from CKAN metadata retrieved from '{url}' on {datetime.datetime.now():%d %b %Y}"
+                }
             }
         }
-    }
 
 
-    xml_string = render_j2_template(mcf_dict, template_dir='../data/templates/ISO19115-3')
-    print(xml_string)
+        xml_string = render_j2_template(mcf_dict, template_dir='../data/templates/ISO19115-3')
+        print(xml_string)
 
-    # write to disk
-    with open('output.xml', 'w') as ff:
-        ff.write(xml_string)
+        # write to disk
+        with open('output.xml', 'w') as ff:
+            ff.write(xml_string)
 
 
-def get_ckan_dict(ckan_url, package_id):
-    url_path =  Path('api') / '3' / 'action' / 'package_show'
-    url = f'{ckan_url}/{url_path}'
-    # print("URL=", url)
-    r = requests.get(url, params={'q':'type:report', 'id':package_id})
-    try:
-        dict = json.loads(r.text)
-    except json.JSONDecodeError:
-        return {}, r.url
-    if dict['success'] is True:
-        return dict['result'], r.url
-    return {}, r.url
+    def write_record(self, ckan_url, package_id):
+        # Set up CKAN URL
+        url_path =  Path('api') / '3' / 'action' / 'package_show'
+        url = f'{ckan_url}/{url_path}'
+        # print("URL=", url)
+        r = requests.get(url, params={'q':'type:report', 'id':package_id})
+        try:
+            dict = json.loads(r.text)
+        except json.JSONDecodeError:
+            return False
+        if dict['success'] is True:
+            self.output_xml(dict['result'], r.url)
+            return True
+        return False
+
 
 if __name__ == "__main__":
-    site_url = 'https://geoscience.data.qld.gov.au'
-    ckan_dict, url = get_ckan_dict(site_url,'ds000002')
-    output_xml(ckan_dict, url)    
-
+    SITE__URL = 'https://geoscience.data.qld.gov.au'
+    ce = CkanExtractor()
+    ce.write_record(SITE__URL, 'ds000002')
--- src/oai_extract.py
+++ src/oai_extract.py
@@ -5,131 +5,136 @@ import datetime
 from pygeometa.core import render_j2_template
 from sickle import Sickle
 
-# Get records from Northern Territory Geological Service
-
-# OAI-PMH URL
-OAI_URL = 'https://geoscience.nt.gov.au/gemis/ntgsoai/request'
+from extractor import Extractor
 
 
 
-def output_xml(oai_dict, bbox, model_endpath):
-    mcf_dict = {
-        "mcf": {
-            "version": 1.0
-        },
-        "metadata": {
-            "identifier": "",
-            "language": "en",
-            "charset": "utf8",
-            "parentidentifier": "",
-            "hierarchylevel": "dataset",
-            "datestamp": oai_dict['relation'][2],
-            "dataseturi": oai_dict['source.uri'][0]
-        },
-        "spatial": {
-            "datatype": "tin",
-            "geomtype": "composite"
-        },
-        "identification": {
-            "language": "eng; AUS",
-            "charset": "utf8",
-            "title": {
-                "en": oai_dict['title'][0],
+class OaiExtractor(Extractor):
+
+    def __init__(self, oai_url, output_dir):
+        self.OAI_URL = oai_url
+        self.output_dir = output_dir
+
+    def output_xml(self, oai_dict, bbox, model_src_url):
+        mcf_dict = {
+            "mcf": {
+                "version": 1.0
             },
-            "abstract": {
-                "en": oai_dict['description.abstract'][0],
+            "metadata": {
+                "identifier": "",
+                "language": "en",
+                "charset": "utf8",
+                "parentidentifier": "",
+                "hierarchylevel": "dataset",
+                "datestamp": oai_dict['relation'][2],
+                "dataseturi": oai_dict['source.uri'][0]
             },
-            "dates": {
-                "creation": oai_dict['date.issued'][0],
-                "publication": oai_dict['date.issued'][0]
+            "spatial": {
+                "datatype": "tin",
+                "geomtype": "composite"
             },
-            "keywords": {
-                "default": {
-                    "keywords": {
-                        "en": oai_dict['subject'][0]
+            "identification": {
+                "language": "eng; AUS",
+                "charset": "utf8",
+                "title": {
+                    "en": oai_dict['title'][0],
+                },
+                "abstract": {
+                    "en": oai_dict['description.abstract'][0],
+                },
+                "dates": {
+                    "creation": oai_dict['date.issued'][0],
+                    "publication": oai_dict['date.issued'][0]
+                },
+                "keywords": {
+                    "default": {
+                        "keywords": {
+                            "en": oai_dict['subject'][0]
+                        }
                     }
+                },
+                "topiccategory": [
+                    "geoscientificInformation"
+                ],
+                "extents": {
+                    "spatial": [
+                        {
+                            "bbox": bbox,
+                            "crs": 4326
+                        }
+                    ]
+                },
+                "fees": "None",
+                "accessconstraints": "license",
+                "rights": {
+                    "en": ' '.join(oai_dict['rights']),
+                },
+                "url": oai_dict['source.uri'][0],
+                "status": "completed",
+                "maintenancefrequency": "continual"
+            },
+            "contact": {
+                "distributor": {
+                    "organization": oai_dict['contributor.issuingbody'][0]
                 }
             },
-            "topiccategory": [
-                "geoscientificInformation"
-            ],
-            "extents": {
-                "spatial": [
-                    {
-                        "bbox": bbox,
-                        "crs": 4326
-                    }
-                ]
+            "distribution": {
+                "wms": {
+                    "url": oai_dict['source.uri'][0],
+                    "type": "WWW:LINK",
+                    "rel": "service",
+                    "name": {
+                        "en": ' '.join(oai_dict['type']),
+                    },
+                    "description": {
+                        "en": ' '.join(oai_dict['type']),
+                    },
+                    "function": "download"
+                }
             },
-            "fees": "None",
-            "accessconstraints": "license",
-            "rights": {
-                "en": ' '.join(oai_dict['rights']),
-            },
-            "url": oai_dict['source.uri'][0],
-            "status": "completed",
-            "maintenancefrequency": "continual"
-        },
-        "contact": {
-            "distributor": {
-                "organization": oai_dict['contributor.issuingbody'][0]
-            }
-        },
-        "distribution": {
-            "wms": {
-                "url": oai_dict['source.uri'][0],
-                "type": "WWW:LINK",
-                "rel": "service",
-                "name": {
-                    "en": ' '.join(oai_dict['type']),
+            "dataquality": {
+                "scope": {
+                    "level": "dataset"
                 },
-                "description": {
-                    "en": ' '.join(oai_dict['type']),
-                },
-                "function": "download"
-            }
-        },
-        "dataquality": {
-            "scope": {
-                "level": "dataset"
-            },
-            "lineage": {
-                "statement": f"{oai_dict['source.volume'][0]}\nThis metadata record was reproduced from NTGS GEMIS metadata retrieved from {oai_dict['source.uri'][0]} on {datetime.datetime.now():%d %b %Y}"
+                "lineage": {
+                    "statement": f"{oai_dict['source.volume'][0]}\nThis metadata record was reproduced from NTGS GEMIS metadata retrieved from {oai_dict['source.uri'][0]} on {datetime.datetime.now():%d %b %Y}"
+                }
             }
         }
-    }
+
+        xml_string = render_j2_template(mcf_dict, template_dir='../data/templates/ISO19115-3')
+        print(xml_string)
+
+        # write to disk
+        with open('output.xml', 'w') as ff:
+            ff.write(xml_string)
 
 
-    xml_string = render_j2_template(mcf_dict, template_dir='../data/templates/ISO19115-3')
-    print(xml_string)
 
-    # write to disk
-    with open('output.xml', 'w') as ff:
-        ff.write(xml_string)
+    def write_record(self, bbox, model_endpath, model_src_url):
+        # Convert perm link to OAI-PMH ID 
+        handle_id = '/'.join(model_src_url.split('/')[-2:])
 
+        sickle = Sickle(self.OAI_URL)
+
+        # NB: Some geological fields that are present in GEMIS website are missing from OAI output with 'oai_dc' prefix,
+        # i.e. "Stratigraphy" The 'xoai' prefix will allow extraction of these missing fields but the XML output
+        # would need to be parsed
+        rec = sickle.GetRecord(identifier='oai:geoscience.nt.gov.au:'+handle_id, metadataPrefix='oai_dc')
+
+        oai_dict = rec.get_metadata()
+        for k,v in oai_dict.items():
+            print(k, ': ', v)
+
+        self.output_xml(oai_dict, bbox, model_endpath)       
 
 if __name__ == "__main__":
+    # Get records from Northern Territory Geological Service
+    # OAI-PMH URL
+    OAI__URL = 'https://geoscience.nt.gov.au/gemis/ntgsoai/request'
+    
     # GEMIS permanent link of McArthur 3D model
-    # TODO: Supply this from model config
-    PERM_LINK = 'https://geoscience.nt.gov.au/gemis/ntgsjspui/handle/1/81751'
-
-    # Convert perm link to OAI-PMH ID 
-    handle_id = '/'.join(PERM_LINK.split('/')[-2:])
-
-    sickle = Sickle(OAI_URL)
-
-    # NB: Some geological fields that are present in GEMIS website are missing from OAI output with 'oai_dc' prefix,
-    # i.e. "Stratigraphy" The 'xoai' prefix will allow extraction of these missing fields but the XML output
-    # would need to be parsed
-    rec = sickle.GetRecord(identifier='oai:geoscience.nt.gov.au:'+handle_id, metadataPrefix='oai_dc')
-
-    oai_dict = rec.get_metadata()
-    for k,v in oai_dict.items():
-        print(k, ': ', v)
-
-    # TODO: Supply these 2 from the model config
-    bbox = [154.3, 109.1, -43.9, -10.6]
-    model_endpath = 'mcarthur'
-
-    output_xml(oai_dict, bbox, model_endpath)    
+    MODEL__URL = 'https://geoscience.nt.gov.au/gemis/ntgsjspui/handle/1/81751'
+    oe = OaiExtractor(OAI__URL, 'output')
+    oe.write_record([154.3, 109.1, -43.9, -10.6], 'mcarthur', MODEL__URL)
 
