c563bb93e2e5e2ae6bb4ebd17a2ac47cd26d4cd0
Vincent Fazio <vjf@users.noreply.github.com>
2024-08-16 01:25:01 +0000

    Add command line option to process a group in config file

--- src/process.py
+++ src/process.py
@@ -3,6 +3,7 @@ import os
 import sys
 import glob
 import json
+import argparse
 
 from pyproj import CRS, Transformer
 
@@ -70,9 +71,42 @@ def oaipmh_convert(param_list):
     for params in param_list:
         oe.write_record(**params)
 
-if __name__ == "__main__":
+
+def process_config(config_val):
     # Get cooordinates from geomodels JSON config
     coord_dict = get_model_info()
+
+    if config_val['method'] is None:
+        return
+    param_list = config_val['params']
+
+    # Insert model coordinates
+    for params in param_list:
+        name = params['name']
+        params['bbox'] = coord_dict[name]
+
+    if config_val['method'] == 'PDF':
+        convert(PDFExtractor, param_list)
+
+    elif config_val['method'] == 'CKAN':
+        convert(CkanExtractor, param_list)
+
+    elif config_val['method'] == 'ISO19115-3':
+        convert(ISO19115_3Extractor, param_list)
+
+    elif config_val['method'] == 'ISO19139':
+        convert(ISO19139Extractor, param_list)
+
+    elif config_val['method'] == 'OAIPMH':
+        oaipmh_convert(param_list)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Metarecogen")
+    parser.add_argument('-r', '--record', action='store', help="Specify a record group to generate")
+
+    # Parse command line arguments
+    args = parser.parse_args()
 
     # Create output dir
     if not os.path.exists(OUTPUT_DIR):
@@ -82,26 +116,14 @@ if __name__ == "__main__":
             print(f"ERROR: Cannot create output dir {OUTPUT_DIR}: {oe}")
             sys.exit(1)
 
-    # Loop over datasets and process each one
-    for k, v in CONFIG.items():
-        if v['method'] is None:
-            continue
-        param_list = v['params']
-        for params in param_list:
-            name = params['name']
-            params['bbox'] = coord_dict[name]
-
-        if v['method'] == 'PDF':
-            convert(PDFExtractor, param_list)
-
-        elif v['method'] == 'CKAN':
-            convert(CkanExtractor, param_list)
-
-        elif v['method'] == 'ISO19115-3':
-            convert(ISO19115_3Extractor, param_list)
-
-        elif v['method'] == 'ISO19139':
-            convert(ISO19139Extractor, param_list)
-
-        elif v['method'] == 'OAIPMH':
-            oaipmh_convert(param_list)
+    # Process one record specified on command line
+    if args.record is not None:
+        if args.record in CONFIG:
+            process_config(CONFIG[args.record])
+        else:
+            print(f"ERROR: Cannot find {args.record} in config")
+            sys.exit(1)
+    else:
+        # Loop over config and process each one
+        for k, v in CONFIG.items():
+            process_config(v)
